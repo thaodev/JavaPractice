e3cdae9fe8baedfdbb0a3b2469fe1e40151ca3f9
Rachel Nguyen <thaontt1408@gmail.com>
2021-03-15 21:27:06 -0600

    I didn't know what the hell I updated

--- src/RecursiveSofArray.java
+++ src/RecursiveSofArray.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class RecursiveSofArray {
-}
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class RecursiveSofArray {
+    public static int sumOfArray(int[] arr, int n) {
+        if (n == 1) return arr[0];
+        return arr[n-1] + sumOfArray(arr, n-1);
+
+    }
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("How many elements in your array? ");
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i=0;i<n;i++) {
+                System.out.println("enter array's elements: ");
+                arr[i] = sc.nextInt();
+            }
+            System.out.print("Sum of you array would be: " + sumOfArray(arr, n));
+        }
+    }
+
--- src/printSquare.java
+++ src/printSquare.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class printSquare {
+import java.util.Scanner;
+
+public class printSquare {
+    public static void square(int min, int max) {
+        for (int i = min; i <= max; i++) {
+            for (int j = i; j <= max; j++) {
+                System.out.print(j);
+            }
+            for (int j = min; j < i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        String answer = "";
+        do {
+            System.out.println("Prepare your min and max for the square");
+            Scanner sc = new Scanner(System.in);
+            System.out.println("What is your min? ");
+            int min = sc.nextInt();
+            System.out.println("What is your max?");
+            int max = sc.nextInt();
+            square(min, max);
+            System.out.println("Do you want to try a different range? ");
+            Scanner res = new Scanner(System.in);
+            answer = res.next();
+
+        } while (answer.toLowerCase().equals("yes") || answer.toLowerCase().equals("y"));
+    }
 }
